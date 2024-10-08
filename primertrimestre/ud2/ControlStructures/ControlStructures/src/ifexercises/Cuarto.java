90fbb5164085c0ef482af72017f5818813b12e7f
danielmoralesar <danielmoralestrb@gmail.com>
2024-10-08 17:25:17 +0200

    Subí carpeta de Primer Trimestre de Programación con la UD2

---
+++ primertrimestre/ud2/ControlStructures/ControlStructures/src/ifexercises/Cuarto.java
@@ -0,0 +1,27 @@
+package ifexercises;
+
+import java.util.Scanner;
+
+public class Cuarto {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        //Solicita un string
+        System.out.println("Ingrese una frase mayor a ocho caracteres");
+        String palabra = scanner.nextLine();
+
+        //Calcula longitud del string
+        int letras = palabra.length();
+
+        //Da el resultado
+        if (letras > 8){
+            System.out.println("Demasiado grande");
+        }
+        else if (letras < 8) {
+            System.out.println("Demasiado pequeño");
+        }
+        else {
+            System.out.println("Válido");
+        }
+    }
+}
---
+++ primertrimestre/ud2/ControlStructures/ControlStructures/src/ifexercises/Sexto.java
@@ -0,0 +1,36 @@
+package ifexercises;
+
+import java.util.Scanner;
+
+public class Sexto {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("¿Qué operación desea realizar? ");
+        String operacion = scanner.nextLine();
+
+        if (operacion.equals("+")) {
+            System.out.println("Ingrese un número con decimales: ");
+            double a = scanner.nextDouble();
+            scanner.nextLine();
+
+            System.out.println("Ingrese otro número con decimales: ");
+            double b = scanner.nextDouble();
+            scanner.nextLine();
+
+            System.out.println("El resultado es: " + (a + b));
+        } else if (operacion.equals("-")) {
+            System.out.println("Ingrese un número decimal: ");
+            int a = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("Ingrese otro número decimal: ");
+            int b = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("El resultado es: " + (a - b));
+        } else {
+            System.out.println("Operación no permitida.");
+        }
+    }
+}
